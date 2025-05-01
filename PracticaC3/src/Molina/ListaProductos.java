09c8c909f35db08bbca0d2033c65f6e3aee23f19
sccps <sccps@NICOLE>
2025-04-30 21:47:41 -0500

    Aporte: Colecciones, Molina Huertas

---
+++ PracticaC3/src/Molina/ListaProductos.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Molina;
+
+import java.util.ArrayList;
+
+public class ListaProductos {
+    //COLECCIONES
+    public static void main(String[] args) {
+        ArrayList<String> productos = new ArrayList<>();
+
+        productos.add("Laptop");
+        productos.add("Monitor");
+        productos.add("Teclado");
+
+        System.out.println("Lista de productos:");
+        for (String producto : productos) {
+            System.out.println("- " + producto);
+        }
+    }
+}
