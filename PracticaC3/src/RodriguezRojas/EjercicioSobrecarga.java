f814e2334d74ab17a9bf0beb150b8c8cae0a6e29
sebasR14 <dierodriguez43@gmail.com>
2025-05-02 17:25:28 -0600

    Update EjercicioSobrecarga.java

--- PracticaC3/src/RodriguezRojas/EjercicioSobrecarga.java
+++ PracticaC3/src/RodriguezRojas/EjercicioSobrecarga.java
@@ -8,17 +8,17 @@ public class EjercicioSobrecarga {
         saludar("Joaquin", 25);
     }
 
-    // Método sin parámetros
+    
     public static void saludar() {
         System.out.println("¡Hola!");
     }
 
-    // Método con un parámetro
+    
     public static void saludar(String nombre) {
         System.out.println("¡Hola, " + nombre + "!");
     }
 
-    // Método con dos parámetros
+   
     public static void saludar(String nombre, int edad) {
         System.out.println("¡Hola, " + nombre + "! Tienes " + edad + " años.");
     }
